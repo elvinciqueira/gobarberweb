5ae5fc41f1e83d2f131e1c4c6f79b5abb5904025
elvinciqueira <elvinciqueira@gmail.com>
2020-05-24 16:01:01 -0300

    feat: agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 import { useAuth } from '../../hooks/auth';
 
 const logoImg = require('../../assets/images/logo.svg') as string;
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +40,90 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/42623554?s=460&u=9b1b99f6d7ea6a615bca0daae9d4d636ce2e7448&v=4"
+                alt="Elvin Ciqueira"
+              />
+
+              <strong>Elvin Ciqueira</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/42623554?s=460&u=9b1b99f6d7ea6a615bca0daae9d4d636ce2e7448&v=4"
+                  alt="Elvin Ciqueira"
+                />
+
+                <strong>Elvin Ciqueira</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/42623554?s=460&u=9b1b99f6d7ea6a615bca0daae9d4d636ce2e7448&v=4"
+                  alt="Elvin Ciqueira"
+                />
+
+                <strong>Elvin Ciqueira</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/42623554?s=460&u=9b1b99f6d7ea6a615bca0daae9d4d636ce2e7448&v=4"
+                  alt="Elvin Ciqueira"
+                />
+
+                <strong>Elvin Ciqueira</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 
 export const Header = styled.header`
-  background: #28262e;
+  background-color: #28262e;
   padding: 32px 0;
 `;
 
@@ -55,4 +55,154 @@ export const Profile = styled.div`
       color: #ff9000;
     }
   }
+`;
+
+export const Content = styled.main`
+  display: flex;
+  margin: 64px auto;
+  max-width: 1120px;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    align-items: center;
+    color: #ff9000;
+    display: flex;
+    font-weight: 500;
+    margin-top: 8px;
+
+    span {
+      align-items: center;
+      display: flex;
+    }
+
+    span + span::before {
+      content: '';
+      background-color: #ff9000;
+      height: 12px;
+      margin: 0 8px;
+      width: 1px;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    align-items: center;
+    background-color: #3e3b47;
+    border-radius: 10px;
+    display: flex;
+    margin-top: 24px;
+    padding: 16px 24px;
+    position: relative;
+
+    &::before {
+      content: '';
+      background-color: #ff9000;
+      height: 80%;
+      left: 0;
+      position: absolute;
+      top: 10%;
+      width: 1px;
+    }
+
+    img {
+      border-radius: 50%;
+      height: 80px;
+      width: 80px;
+    }
+
+    strong {
+      color: #fff;
+      margin-left: 24px;
+    }
+
+    span {
+      align-items: center;
+      color: #999591;
+      display: flex;
+      margin-left: auto;
+
+      svg {
+        color: #ff9000;
+        margin-right: 8px;
+      }
+    }
+  }
+`;
+
+export const Section = styled.section`
+  margin-top: 48px;
+
+  > strong {
+    color: #999591;
+    border-bottom: 1px solid #3e3b47;
+    display: block;
+    font-size: 20px;
+    line-height: 26px;
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+  }
+`;
+
+export const Appointment = styled.div`
+  align-items: center;
+  display: flex;
+
+  & + div {
+    margin-top: 16px;
+  }
+
+  span {
+    align-items: center;
+    color: #f4ede8;
+    display: flex;
+    margin-left: auto;
+
+    svg {
+      color: #ff9000;
+      margin-right: 8px;
+    }
+  }
+
+  div {
+    align-items: center;
+    background-color: #3e3b47;
+    border-radius: 10px;
+    display: flex;
+    flex: 1;
+    margin-left: 24px;
+    padding: 16px 24px;
+
+    img {
+      border-radius: 50%;
+      height: 56px;
+      width: 56px;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 20px;
+      margin-left: 24px;
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
 `;
